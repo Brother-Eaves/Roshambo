8145c1d341af291b14a4d5fcad83924cb2517543
Brother-Eaves <david.g.t.eaves@gmail.com>
2024-11-26 21:00:43 -0500

    Initilaize computer choice

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // TOP Paper, Rock, Scissors
 // Initialize Computer Choice
+
+const getComputerChoice = () => {
+   let result = Math.floor(Math.random() * 3) + 1;
+    if (result === 1) {
+        console.log('Rock')
+    } else if (result === 2) {
+        console.log('Paper')
+    } else if (result === 3) 
+        console.log('Scissors')
+}
+
+console.log(getComputerChoice())
+
 //Function Randomize a number between 1 and three.
 // If 1 = rock, else if 2 = Paper else scissors.
 //Return result
