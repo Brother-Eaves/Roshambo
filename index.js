a94853656d4186dca364e78aad8668735ceff5ce
Brother-Eaves <david.g.t.eaves@gmail.com>
2024-11-26 20:50:01 -0500

    Initialize Pseudocode

---
+++ index.js
@@ -0,0 +1,8 @@
+// TOP Paper, Rock, Scissors
+// Initialize Computer Choice
+//Function Randomize a number between 1 and three.
+// If 1 = rock, else if 2 = Paper else scissors.
+//Return result
+//Initialize Human Choice
+//Function prompt choice
+//Evaluate results: Rock beats Scissors/Paper beats Rock/Scissors beat paper. 
